35e6ede395dba9bdbccf3ca458501794fe7bf530
unnamed5719 <1529153949@qq.com>
2016-08-12 20:37:32 +0800

    Init Commit

---
+++ demo.py
@@ -0,0 +1,25 @@
+import urllib.request
+import re
+import os
+import time
+
+loop = 0
+isliveurl = 'http://live.bilibili.com/bili/isliving/123484'
+islive = str(urllib.request.urlopen(isliveurl).read())
+try:
+    req = re.findall(r"(url)",islive)[0]
+except:
+    print('还没直播呐')
+    exit()
+else:
+    url = 'http://live.bilibili.com/api/h5playurl?roomid=33616'
+    while(True):
+        r = str(urllib.request.urlopen(url).read())
+        req = re.findall(r"(http://[^\s]{,58})",r)[0]
+        loop = loop + 1
+        os.system('ffmpeg -i '+ req +' -acodec copy -bsf:a aac_adtstoasc -vcodec copy -f mp4 '+ str(int(time.time())) +'.mp4')
+        time.sleep(30)
+        if loop ==10:
+            break
+        else:
+           pass
